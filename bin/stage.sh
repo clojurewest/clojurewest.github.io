ebfde683e05579eaf83a0230970740083ed13d2f
puredanger <alex.miller@cognitect.com>
2016-01-04 14:46:03 -0600

    stage cfp

--- bin/stage.sh
+++ bin/stage.sh
@@ -7,7 +7,7 @@ aws s3 sync _site/ s3://staging.clojurewest.org --delete
 
 # redirects
 touch _site/empty
-for page in index sponsorship policies speakers speakers-2015 opportunity training
+for page in index sponsorship policies speakers speakers-2015 opportunity training cfp
 do
 	aws s3 cp _site/empty s3://staging.clojurewest.org/${page} --website-redirect /${page}.html
 done
